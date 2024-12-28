d4c05f2a0c1a1adc25d55766be758b1dd67c882b
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-12-29 01:58:52 +0530

    Added stats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Stats from './components/Stats';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       
       <Navbar />
       <Header/>
+      <Stats/>
       
     </div>
   );
---
+++ src/components/Stats.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BiSolidBriefcase, BiSolidGroup, BiSolidBank } from 'react-icons/bi';
+
+const data = [
+    {
+        title: "Live Jobs",
+        icon: <BiSolidBriefcase className='size-8' />,
+        value: "31,091"
+    },
+    {
+        title: "Companies",
+        icon: <BiSolidBank className='size-8' />,
+        value: "19,879"
+    },
+    {
+        title: "Candidatess",
+        icon: <BiSolidGroup className='size-8' />,
+        value: "1,94,180"
+    },
+    {
+        title: "New Jobs",
+        icon: <BiSolidBriefcase className='size-8' />,
+        value: "15,153"
+    },
+
+];
+
+
+const Stats = () =>
+(
+    <section className="flex items-center justify-center flex-wrap gap-x-4 lg:gap-20 my-8 lg:my-0">
+        {data.map((item, index) => (
+            <div className="flex items-center p-2 lg:p-5 rounded-lg gap-2">
+                <span className="hidden mr-2 text-teal-500 bg-teal-50 sm:inline p-3 rounded-lg">
+                    {item.icon}
+                </span>
+                <div className="flex flex-col items-start">
+                    <p className="block text-md md:text-xl font-bold text-gray-900 lg:text-2xl mb-0" >
+                        {item.value}
+                    </p> <span className="text-xs md:text-sm text-gray-500">
+                        {item.title}</span>
+                </div>
+            </div>
+        ))}
+    </section>
+);
+
+export default Stats;
